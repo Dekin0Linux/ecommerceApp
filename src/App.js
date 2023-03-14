7843c2f277a6540013f0176b0b2198a5614cf8a5
dekin <dekinlinux@gmail.com>
2023-03-14 06:59:02 +0000

    product Detail

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <div className='bg-slate-200'>
-      <Home/>
+      {/* <Home/> */}
+      <ProductDetail/>
       <Footer/>
     </div>
   );
