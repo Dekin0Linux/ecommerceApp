c0282e07632e735c7dabb260017fe8ddd4284f48
dekin <dekinlinux@gmail.com>
2023-03-23 14:07:37 +0000

    fixes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaCalendarAlt,FaTruckMoving,FaCreditCard,FaShoppingCart} from 'react-icons/fa'
+import {FaShoppingCart} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 
